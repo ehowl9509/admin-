b95f294c9f7521c6d66d54214ce594daf19edf62
ehowl9509 <ehowl9509@naver.com>
2019-08-23 12:25:02 +0900

    entity 수정 추가

--- src/main/java/com/example/admin1/model/Item.java
+++ src/main/java/com/example/admin1/model/Item.java
@@ -6,6 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -13,7 +17,32 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String status;
+
+    private String name;
+
+    private String title;
+
+    private String content;
+
+    private Integer price;
+
+    private String  brandName;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
     
 }
