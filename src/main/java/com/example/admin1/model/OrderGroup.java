f9d1bc31198241877ab19db38bf5bfb8bb86d3af
ehowl9509 <ehowl9509@naver.com>
2019-08-26 09:07:52 +0900

    오더그룹추가

---
+++ src/main/java/com/example/admin1/model/OrderGroup.java
@@ -0,0 +1,50 @@
+package com.example.admin1.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class OrderGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String status;
+
+    private String orderType;
+
+    private String revAddress;
+
+    private String revName;
+
+    private String paymentType;
+
+    private BigDecimal totalprice;
+
+    private Integer totalQuantity;
+
+    private LocalDateTime orderAt;
+
+    private LocalDateTime arrivalDate;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+}
