b4924113de8feb64af650bab3e1603f23926b726
ehowl9509 <ehowl9509@naver.com>
2019-08-26 11:09:37 +0900

    내용 수정

--- src/main/java/com/example/admin1/model/AdminUser.java
+++ src/main/java/com/example/admin1/model/AdminUser.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
@@ -14,6 +17,8 @@ import java.time.LocalDateTime;
 @Entity
 public class AdminUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String account;
