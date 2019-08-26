307d35d12e602a21b1c606cdbb2239cca35d99f7
ehowl9509 <ehowl9509@naver.com>
2019-08-26 12:26:28 +0900

    오류수정

--- src/main/java/com/example/admin1/repository/CategoryRepository.java
+++ src/main/java/com/example/admin1/repository/CategoryRepository.java
@@ -1,9 +1,14 @@
 package com.example.admin1.repository;
 
 
+import com.example.admin1.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CategoryRepository extends JpaRepository<CategoryRepository, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByType(String type);
 }
